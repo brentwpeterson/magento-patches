f8c6dad0389060680046bb1ed8e6434fdb818915
Brent Peterson <brent@wagento.com>
2015-12-01 08:04:43 -0600

    update sa commands

--- magento-support-utilities/sacommands.sh
+++ magento-support-utilities/sacommands.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-mysql -e "show global status" >>text.txt
-mysql -e "show global variables" >>text.txt
-netstat -nap >>text.txt
-iostat -dx >>text.txt
-dmesg >>text.txt
-free -mo >>text.txt
-uptime >>text.txt
-php -v >>text.txt
-php -m >>text.txt
-php -i >>text.txt
+vmstat 1 10 >sa_output.txt
+mysql -e "show global status" >>sa_output.txt
+mysql -e "show global variables" >>sa_output.txt
+netstat -nap >>sa_output.txt
+iostat -dx >>sa_output.txt
+dmesg >>sa_output.txt
+free -mo >>sa_output.txt
+uptime >>sa_output.txt
+crontab -l >>sa_output.txt
+php -v >>sa_output.txt
+php -m >>sa_output.txt
+php -i >>sa_output.txt
+ls -latr app app/etc var >>sa_output.txt
