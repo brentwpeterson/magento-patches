a7998f3d3dd2a0799f2023cbed79bedd49873f0e
Jeroen Boersma <jeroen@elgentos.nl>
2020-06-25 09:49:05 +0200

    Added SUPEE-11346 M1

---
+++ CE1.9/PATCH_SUPEE-11346_CE_1.9.1.1_v1.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+# Patch apllying tool template
+# v0.1.2
+# (c) Copyright 2013. Magento Inc.
+#
+# DO NOT CHANGE ANY LINE IN THIS FILE.
+
+# 1. Check required system tools
+_check_installed_tools() {
+    local missed=""
+
+    until [ -z "$1" ]; do
+        type -t $1 >/dev/null 2>/dev/null
+        if (( $? != 0 )); then
+            missed="$missed $1"
+        fi
+        shift
+    done
+
+    echo $missed
+}
+
+REQUIRED_UTILS='sed patch'
+MISSED_REQUIRED_TOOLS=`_check_installed_tools $REQUIRED_UTILS`
+if (( `echo $MISSED_REQUIRED_TOOLS | wc -w` > 0 ));
+then
+    echo -e "Error! Some required system tools, that are utilized in this sh script, are not installed:\nTool(s) \"$MISSED_REQUIRED_TOOLS\" is(are) missed, please install it(them)."
+    exit 1
+fi
+
+# 2. Determine bin path for system tools
+CAT_BIN=`which cat`
+PATCH_BIN=`which patch`
+SED_BIN=`which sed`
+PWD_BIN=`which pwd`
+BASENAME_BIN=`which basename`
+
+BASE_NAME=`$BASENAME_BIN "$0"`
+
+# 3. Help menu
+if [ "$1" = "-?" -o "$1" = "-h" -o "$1" = "--help" ]
+then
+    $CAT_BIN << EOFH
+Usage: sh $BASE_NAME [--help] [-R|--revert] [--list]
+Apply embedded patch.
+
+-R, --revert    Revert previously applied embedded patch
+--list          Show list of applied patches
+--help          Show this help message
+EOFH
+    exit 0
+fi
+
+# 4. Get "revert" flag and "list applied patches" flag
+REVERT_FLAG=
+SHOW_APPLIED_LIST=0
+if [ "$1" = "-R" -o "$1" = "--revert" ]
+then
+    REVERT_FLAG=-R
+fi
+if [ "$1" = "--list" ]
+then
+    SHOW_APPLIED_LIST=1
+fi
+
+# 5. File pathes
+CURRENT_DIR=`$PWD_BIN`/
+APP_ETC_DIR=`echo "$CURRENT_DIR""app/etc/"`
+APPLIED_PATCHES_LIST_FILE=`echo "$APP_ETC_DIR""applied.patches.list"`
+
+# 6. Show applied patches list if requested
+if [ "$SHOW_APPLIED_LIST" -eq 1 ] ; then
+    echo -e "Applied/reverted patches list:"
+    if [ -e "$APPLIED_PATCHES_LIST_FILE" ]
+    then
+        if [ ! -r "$APPLIED_PATCHES_LIST_FILE" ]
+        then
+            echo "ERROR: \"$APPLIED_PATCHES_LIST_FILE\" must be readable so applied patches list can be shown."
+            exit 1
+        else
+            $SED_BIN -n "/SUP-\|SUPEE-/p" $APPLIED_PATCHES_LIST_FILE
+        fi
+    else
+        echo "<empty>"
+    fi
+    exit 0
+fi
+
+# 7. Check applied patches track file and its directory
+_check_files() {
+    if [ ! -e "$APP_ETC_DIR" ]
+    then
+        echo "ERROR: \"$APP_ETC_DIR\" must exist for proper tool work."
+        exit 1
+    fi
+
+    if [ ! -w "$APP_ETC_DIR" ]
+    then
+        echo "ERROR: \"$APP_ETC_DIR\" must be writeable for proper tool work."
+        exit 1
+    fi
+
+    if [ -e "$APPLIED_PATCHES_LIST_FILE" ]
+    then
+        if [ ! -w "$APPLIED_PATCHES_LIST_FILE" ]
+        then
+            echo "ERROR: \"$APPLIED_PATCHES_LIST_FILE\" must be writeable for proper tool work."
+            exit 1
+        fi
+    fi
+}
+
+_check_files
+
+# 8. Apply/revert patch
+# Note: there is no need to check files permissions for files to be patched.
+# "patch" tool will not modify any file if there is not enough permissions for all files to be modified.
+# Get start points for additional information and patch data
+SKIP_LINES=$((`$SED_BIN -n "/^__PATCHFILE_FOLLOWS__$/=" "$CURRENT_DIR""$BASE_NAME"` + 1))
+ADDITIONAL_INFO_LINE=$(($SKIP_LINES - 3))p
+
+_apply_revert_patch() {
+    DRY_RUN_FLAG=
+    if [ "$1" = "dry-run" ]
+    then
+        DRY_RUN_FLAG=" --dry-run"
+        echo "Checking if patch can be applied/reverted successfully..."
+    fi
+    PATCH_APPLY_REVERT_RESULT=`$SED_BIN -e '1,/^__PATCHFILE_FOLLOWS__$/d' "$CURRENT_DIR""$BASE_NAME" | $PATCH_BIN $DRY_RUN_FLAG $REVERT_FLAG -p0`
+    PATCH_APPLY_REVERT_STATUS=$?
+    if [ $PATCH_APPLY_REVERT_STATUS -eq 1 ] ; then
+        echo -e "ERROR: Patch can't be applied/reverted successfully.\n\n$PATCH_APPLY_REVERT_RESULT"
+        exit 1
+    fi
+    if [ $PATCH_APPLY_REVERT_STATUS -eq 2 ] ; then
+        echo -e "ERROR: Patch can't be applied/reverted successfully."
+        exit 2
+    fi
+}
+
+REVERTED_PATCH_MARK=
+if [ -n "$REVERT_FLAG" ]
+then
+    REVERTED_PATCH_MARK=" | REVERTED"
+fi
+
+_apply_revert_patch dry-run
+_apply_revert_patch
+
+# 9. Track patch applying result
+echo "Patch was applied/reverted successfully."
+ADDITIONAL_INFO=`$SED_BIN -n ""$ADDITIONAL_INFO_LINE"" "$CURRENT_DIR""$BASE_NAME"`
+APPLIED_REVERTED_ON_DATE=`date -u +"%F %T UTC"`
+APPLIED_REVERTED_PATCH_INFO=`echo -n "$APPLIED_REVERTED_ON_DATE"" | ""$ADDITIONAL_INFO""$REVERTED_PATCH_MARK"`
+echo -e "$APPLIED_REVERTED_PATCH_INFO\n$PATCH_APPLY_REVERT_RESULT\n\n" >> "$APPLIED_PATCHES_LIST_FILE"
+
+exit 0
+
+
+SUPEE-11346_CE_1911 | CE_1.9.1.1 | v1 | 3bdebbd936d867028c8bc7df1f5f6a3497feb1e6 | Thu Jun 11 18:28:14 2020 +0000 | c4cf5dfdc53d6f6609d1f6bdacf79018dd8f912b..HEAD
+
+__PATCHFILE_FOLLOWS__
+diff --git app/code/core/Mage/Adminhtml/Block/Widget/Form.php app/code/core/Mage/Adminhtml/Block/Widget/Form.php
+index 7c4414b917e..f018954575f 100644
+--- app/code/core/Mage/Adminhtml/Block/Widget/Form.php
++++ app/code/core/Mage/Adminhtml/Block/Widget/Form.php
+@@ -190,7 +190,7 @@ class Mage_Adminhtml_Block_Widget_Form extends Mage_Adminhtml_Block_Widget
+                         'label'     => $attribute->getFrontend()->getLabel(),
+                         'class'     => $attribute->getFrontend()->getClass(),
+                         'required'  => $attribute->getIsRequired(),
+-                        'note'      => $attribute->getNote(),
++                        'note'      => $this->escapeHtml($attribute->getNote()),
+                     )
+                 )
+                 ->setEntityAttribute($attribute);
+diff --git app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php
+index ef1b7b7212c..90bd666876b 100644
+--- app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php
++++ app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php
+@@ -267,6 +267,7 @@ class Mage_Adminhtml_Catalog_Product_AttributeController extends Mage_Adminhtml_
+             if(!isset($data['apply_to'])) {
+                 $data['apply_to'] = array();
+             }
++            $data['entity_type_id'] = $model->getEntityTypeId();
+ 
+             //filter
+             $data = $this->_filterPostData($data);
+diff --git app/code/core/Mage/Eav/Model/Attribute.php app/code/core/Mage/Eav/Model/Attribute.php
+index 9bae0339a7b..04fc3e1315d 100644
+--- app/code/core/Mage/Eav/Model/Attribute.php
++++ app/code/core/Mage/Eav/Model/Attribute.php
+@@ -116,7 +116,7 @@ abstract class Mage_Eav_Model_Attribute extends Mage_Eav_Model_Entity_Attribute
+         if (is_array($rules)) {
+             return $rules;
+         } else if (!empty($rules)) {
+-            return unserialize($rules);
++            return Mage::helper('core/unserializeArray')->unserialize($rules);
+         }
+         return array();
+     }
